641f0f8d61a15608efea8b6f7ac36a6edb72f874
Agustina Fennema <agusfennema@gmail.com>
2025-06-16 16:00:11 -0300

    ads

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AnimalesComponent } from './components/animales/animales.component';
+import { AnimalFormComponent } from './components/animal-form/animal-form.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'animales', component: AnimalesComponent }
+];
